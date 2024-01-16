4fc59369d10bad7c16bdf2481779a6209e9edcc7
Gaetano <gaem195@gmail.com>
2024-01-16 16:53:59 +0100

    add jobs useSelector

--- src/components/MainSearch.jsx
+++ src/components/MainSearch.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import Job from "./Job";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getJobsAction } from "../redux/actions";
 
 const MainSearch = () => {
   const [query, setQuery] = useState("");
   // const [jobs, setJobs] = useState([])
 
+  const jobs = useSelector((state) => state.job.jobsList);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
